525b30cf8613070ff74564d0e447c6a1a40f3e7c
Ryan-McD <Ryan-McD@users.noreply.github.com>
2023-04-09 22:03:17 -0400

    feat: add harbor

---
+++ hack/scale-down-nfs-deployments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+nfs_deployments=$(kubectl get deploy --all-namespaces -ojsonpath='{range .items[?(@.spec.template.spec.volumes[*].nfs)]}{.metadata.namespace}{" "}{.metadata.name}{"\n"}' | head -n -1)
+declare -a arr=($nfs_deployments)
+len=${#arr[*]}
+
+for (( i=0; i<$len; i++ )); do \
+  f=$i+1; \
+  kubectl -n ${arr[$i]} scale --replicas=0 deployment ${arr[$f]}; \
+  i=$f; \
+done
+
+kubectl -n monitoring scale --replicas=0 deployment.apps/grafana
+kubectl -n development scale --replicas=0 sts --all
